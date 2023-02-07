c5ccee21fb3b61b9ccd1f287d0764545eec8ca38
aktive <dragonapp2000@gmail.com>
2023-02-07 13:34:33 +0100

    dont know again

---
+++ src/main/kotlin/Narrator.kt
@@ -0,0 +1,91 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+var narrationModifier: (String) -> String = { it}
+
+fun narrate(
+    message: String,
+    modifier: (String) -> String = { narrationModifier(it) }
+) {
+    println(modifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+
+    //when (Random.nextInt(1..4)) {
+    when (6) {
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoints = 3
+                message.uppercase() + "!".repeat(numExclamationPoints)
+            }
+        }
+
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace(" ", "... ")
+            }
+        }
+
+        3 -> {
+            mood = "unsure"
+            modifier = { message ->
+                "$message?"
+            }
+        }
+
+        4 -> {
+            mood = "lazy"
+            modifier = { message ->
+                "${message.substring(0, message.length / 2)}"
+            }
+        }
+
+        5 -> {
+            mood = "1337"
+            val regex: Regex = """[aeilotsm]""".toRegex()
+            modifier = { message ->
+                message.toLowerCase().replace(regex) { m ->
+                    when(m.value) {
+                        "a" -> "4"
+                        "e" -> "3"
+                        "i" -> "1"
+                        "l" -> "1"
+                        "o" -> "0"
+                        "t" -> "7"
+                        "s" -> "5"
+                        "m" -> "///"
+                        else -> ""
+                    }
+                }
+            }
+        }
+
+        6 -> {
+            mood = "poetic"
+            val randomBreaks: Int = Random.nextInt(0..2)
+            modifier = { message ->
+                when(randomBreaks) {
+                    0 -> message
+                    1 -> message + "\n"
+                    2 -> message +"\n\n"
+                    else -> message
+                }
+
+            }
+        }
+
+        else -> {
+            mood = "professional"
+            modifier = { message ->
+                "$message"
+            }
+        }
+    }
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
+}
--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -1,2 +1,35 @@
+import java.security.ProtectionDomain
+
 class NyetHack {
+
+}
+
+fun main() {
+    narrate("A hero enters the town of Kronstadt. What is their name?",
+    ::makeYellow
+    )
+    /*
+    { message ->
+    // Prints the message in yellow
+        "\u001b[33;1m$message\u001b[0m"
+} */
+    val heroName = readLine()
+    require(heroName != null && heroName.isNotEmpty()) {
+        "The hero must have a name"
+    }
+
+    changeNarratorMood()
+    narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+}
+
+private fun makeYellow(message: String) = "\u001B[33;1m$message\u001B[0m"
+private fun createTitle(name: String): String {
+    return when {
+        name.all { it.isDigit() } -> "The Identifiable"
+        name.none { it.isLetter()} -> "The witness protection member"
+        name.count() { it.lowercase() in "aeiou" } > 4 -> "The Master of vowels"
+        name.all { it.isUpperCase() } -> "The Bold"
+        name.count() > 10 -> "The Verbose"
+        else -> "The Renowned Hero"
+    }
 }
