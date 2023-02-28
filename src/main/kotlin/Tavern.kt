da1e4285ce751bc295514a0019461b5cb11905ab
aktive <dragonapp2000@gmail.com>
2023-02-28 16:23:53 +0100

    chapter 9 2nd challenge

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -10,10 +10,16 @@ private val menuData = File("data/tavern-menu-data.txt")
     .readText()
     .split("\n")
 
-private val menuItems = List(menuData.size) { index ->
+private val menuItemsNames = List(menuData.size) { index ->
     val (_, name, _) = menuData[index].split(",")
     name
 }
+
+private var menuItems = List(menuData.size) { index ->
+    val (category, name, price) = menuData[index].split(",")
+    listOf(category, name, price)
+}
+
 
 private val menuPrices = List(menuData.size) { index ->
     val(_,_, price) = menuData[index].split(",")
@@ -23,21 +29,36 @@ private val menuPrices = List(menuData.size) { index ->
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
     //narrate("There are several items for sale:")
-    narrate("Welcome to Taernyl's Folly")
+    narrate("*** Welcome to Taernyl's Folly ***")
 
+
+    // Sort the list of menu items
+    menuItems = menuItems.sortedBy { it[0] }
     //get longest menu item.
-    val longestItem = menuItems.maxBy { it.length }.length
+    val longestItem = menuItemsNames.maxBy { it.length }.length
 
     //longest price
     val longestPrice = menuPrices.maxBy { it.length}.length
-    println("longest price $longestPrice")
 
     // row length
     val rowLength = longestItem + longestPrice + 5
 
 
     //loop print menu item dots calculated price
-    menuItems.forEachIndexed {index, item ->
+    menuItems.forEachIndexed { index, item ->
+        val dots = rowLength - (item[1].length + item[2].length)
+        print(item[1])
+        repeat(dots) {
+            print(".")
+        }
+        print(item[2]) // Price
+        println()
+        print("~[${item[0]}]~") // Category
+        println()
+
+    }
+    /*
+    menuItemsNames.forEachIndexed { index, item ->
         val dots = rowLength - (item.length + menuPrices[index].length)
         print(item)
         repeat(dots) {
@@ -46,6 +67,7 @@ fun visitTavern() {
         print(menuPrices[index])
         println()
     }
+     */
 
     val patrons: MutableSet<String> = mutableSetOf()
     while(patrons.size < 10) {
@@ -57,7 +79,7 @@ fun visitTavern() {
     narrate(patrons.joinToString())
 
     repeat(3) {
-        placeOrder(patrons.random(), menuItems.random())
+        placeOrder(patrons.random(), menuItemsNames.random())
     }
 
 
