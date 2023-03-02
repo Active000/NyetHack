f8f3211a2427ed87ab7b7582b1abb71052545646
aktive <dragonapp2000@gmail.com>
2023-03-02 15:43:35 +0100

    challenge chapter 10

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -20,6 +20,15 @@ private var menuItems = List(menuData.size) { index ->
     listOf(category, name, price)
 }
 
+private val menuItemPrices: Map<String, Double> = List(menuData.size) { index ->
+    val (_, name, price) = menuData[index].split(",")
+    name to price.toDouble()
+}.toMap()
+
+private val menuItemTypes: Map<String, String> = List(menuData.size) { index ->
+    val ( type,name, _) = menuData[index].split(",")
+    name to type
+}.toMap()
 
 private val menuPrices = List(menuData.size) { index ->
     val(_,_, price) = menuData[index].split(",")
@@ -88,18 +97,32 @@ fun visitTavern() {
         placeOrder(patrons.random(), menuItemsNames.random(), patronGold)
     }
 
+    displayPatronBalances(patronGold)
 
 }
+
+private fun displayPatronBalances(patronGold: Map<String, Double>) {
+    narrate("$heroName intuitively knows how much money each patron has")
+    patronGold.forEach { (patron, balance) ->
+        narrate("$patron has ${"%.2f".format(balance)} gold")
+    }
+}
+
 
 private fun placeOrder(
     patronName: String,
     menuItemName: String ,
     patronGold: MutableMap<String, Double>
 ) {
-    val itemPrice = 1.0
+    val itemPrice = menuItemPrices.getValue(menuItemName)
     narrate("$patronName speaks with $TAVERN_MASTER to place an order")
     if (itemPrice <= patronGold.getOrDefault(patronName, 0.0)) {
-        narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
+        val action = when(menuItemTypes[menuItemName]) {
+            "shandy", "elixer" -> "pours"
+            "meal" -> "serves"
+            else -> "hands"
+        }
+        narrate("$TAVERN_MASTER $action $patronName a $menuItemName")
         narrate("$patronName pays $TAVERN_MASTER $itemPrice gold")
         patronGold[patronName] = patronGold.getValue(patronName) - itemPrice
         patronGold[TAVERN_MASTER] = patronGold.getValue(TAVERN_MASTER) + itemPrice
